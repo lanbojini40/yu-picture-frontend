c9141c05ca491ca95dc5124b64b3dccc975aea25
lanbojini40 <1330399793@qq.com>
2024-12-17 21:31:13 +0800

    项目提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <div id="app">
+  <BasicLayout />
+  </div>
+  </template>
+
+  <script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+//已经改为在权限校验文件中获取
+// import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+// const loginUserStore = useLoginUserStore()
+// loginUserStore.fetchLoginUser()
+
+</script>
+<style>
+#app {
+}
+</style>
+
